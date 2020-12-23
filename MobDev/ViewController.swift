54dd6c4d191dad0a7f31d2bbc4416136bcbb15ce
Ononim <polzyvatel.artur@gmail.com>
2020-12-23 16:06:29 +0200

    Initial Commit

---
+++ MobDev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobDev
+//
+//  Created by Artur Yovenko on 22.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
